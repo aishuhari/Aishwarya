1e8ba7c991a7c2c35b6e8d6d81b81f3fc6e31920
aishuhari <112374611+aishuhari@users.noreply.github.com>
2023-01-30 17:16:54 +0530

    Add files via upload

---
+++ 5-Task/NandA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Task
+{
+    internal class NandA
+    {
+        string name;
+        int age;
+        public NandA()
+        {
+            name = "Vishwa";
+            age = 20;
+        }
+        public NandA(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public static void Main(string[] args)
+        {
+            NandA obj = new NandA();
+            Console.WriteLine(obj.name);
+            Console.WriteLine(obj.age);
+            NandA bobj = new NandA("Joy", 21);
+            Console.WriteLine(bobj.name);
+            Console.WriteLine(bobj.age);
+        }
+
+    }
+}
