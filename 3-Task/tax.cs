3963cb4e8c37cfd7b1cc2ad2e5f5f6d070ca610c
aishuhari <112374611+aishuhari@users.noreply.github.com>
2023-01-30 17:15:28 +0530

    Add files via upload

---
+++ 3-Task/tax.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Task
+{
+    internal class tax
+    {
+        class BaseClass
+        {
+            public void CalculateTax()
+            {
+                double vat = 0.2;
+                double price = 100;
+                double tax = price * vat;
+                Console.WriteLine("VAT: " + tax);
+            }
+        }
+
+        class DerivedClass : BaseClass
+        {
+            public new void CalculateTax()
+            {
+                double gst = 0.12;
+                double price = 100;
+                double tax = price * gst;
+                Console.WriteLine("GST: " + tax);
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                BaseClass baseClass = new BaseClass();
+                baseClass.CalculateTax();
+
+                DerivedClass derivedClass = new DerivedClass();
+                derivedClass.CalculateTax();
+
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
