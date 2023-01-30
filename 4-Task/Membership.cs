2dcfa41f23d0b1069abf6e7dc6d471e6aedbe50a
aishuhari <112374611+aishuhari@users.noreply.github.com>
2023-01-30 17:16:14 +0530

    Add files via upload

---
+++ 4-Task/Membership.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Task
+{
+    public abstract class Membership
+    {
+
+        public abstract void membershipType();
+
+    }
+    public class Silver_membership : Membership
+    {
+        public override void membershipType()
+        {
+            Console.WriteLine("3 days accomodation at resort ");
+        }
+    }
+    public class Gold_Membership : Membership
+    {
+        public override void membershipType()
+        {
+            Console.WriteLine("5 days accomodation at resort and 2 dinners on the house ");
+        }
+    }
+    public class Platinum_Membership : Membership
+    {
+        public override void membershipType()
+        {
+            Console.WriteLine("7 days accomodation at resort and 5 dinners on the house ");
+        }
+    }
+    public class MembershipImplementation
+    {
+        public static void Main(string[] args)
+        {
+            MembershipImplementation m1 = new MembershipImplementation();
+            string membership_choice = "";
+            Console.WriteLine("Enter the membership choice either silver,gold,platinum:");
+            membership_choice = Console.ReadLine();
+
+            Membership membership;
+            switch (membership_choice)
+            {
+                case "silver":
+                    membership = new Silver_membership();
+                    break;
+                case "gold":
+                    membership = new Gold_Membership();
+                    break;
+                case "platinum":
+                    membership = new Platinum_Membership();
+                    break;
+                default:
+                    Console.WriteLine("Invalid membership choice");
+                    return;
+            }
+            membership.membershipType();
+        }
+    }
+}
