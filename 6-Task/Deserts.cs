ff1ab272252d03e1ffee6b3b670de3d1454f71f1
aishuhari <112374611+aishuhari@users.noreply.github.com>
2023-01-30 17:17:33 +0530

    Add files via upload

---
+++ 6-Task/Deserts.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_TASK
+{
+    internal class Deserts
+    {
+        public static void Main()
+        {
+
+            ArrayList dsrts = new ArrayList();
+            dsrts.Add("Browine");
+            dsrts.Add("Waffle");
+            dsrts.Add("Rasmalai");
+            dsrts.Add("Donut");
+            dsrts.Add("Gulab Jamun");
+
+          
+
+            FileStream fs = new FileStream("C:\\Inatech\\Deseerts.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (object i in dsrts)
+            sw.WriteLine(i);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+           
+
+            FileInfo info = new FileInfo("C:\\Inatech\\Deseerts.txt");
+            Console.WriteLine("Creation Time : {0}", info.CreationTime);
+            Console.WriteLine("Size : {0}", info.Length);
+
+
+
+        }
+
+
+    }
+}
---
+++ 6-Task/EmployeeDetails.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_TASK
+{
+    internal class EmployeeDetails
+    {
+        public int EmplId { get; set; }
+        public string? EmplName { get; set; }
+        public string? Emplgender { get; set; }
+        public int Emplage { get; set; }
+
+        public void readfEmp()
+        {
+            FileStream emp = new FileStream("C:\\Inatech\\EmployeeDetails.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(emp);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            EmployeeDetails emp = new EmployeeDetails();
+            Console.WriteLine("Enter Employee Name:");
+            emp.EmplName = Console.ReadLine();
+            Console.WriteLine("Enter Employee Gender:");
+            emp.Emplgender = Console.ReadLine();
+            Console.WriteLine("Enter Employee ID:");
+            emp.EmplId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Age:");
+            emp.Emplage = Convert.ToInt32(Console.ReadLine());
+
+            FileStream Emp = new FileStream("C:\\Inatech\\EmployeeDetails.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(Emp);
+            sw.Write(emp.EmplName);
+            sw.Write(emp.Emplgender);
+            sw.Write(emp.Emplage);
+            sw.Write(emp.EmplId);
+            sw.Flush();
+            sw.Close();
+            Emp.Close();
+            emp.readfEmp();
+        }
+        
+
+
+    }
+}
